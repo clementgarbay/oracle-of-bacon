4a2f5ca7c4fad6e040a86b1433f91483d91920a1
Clément Garbay <clementgarbay@gmail.com>
2018-02-21 09:16:56 +0100

    Implement ES CompletionLoader using bulk processor

--- oracle-of-bacon-backend/src/main/java/com/serli/oracle/of/bacon/loader/elasticsearch/CompletionLoader.java
+++ oracle-of-bacon-backend/src/main/java/com/serli/oracle/of/bacon/loader/elasticsearch/CompletionLoader.java
@@ -1,13 +1,27 @@
 package com.serli.oracle.of.bacon.loader.elasticsearch;
 
 import com.serli.oracle.of.bacon.repository.ElasticSearchRepository;
+import org.elasticsearch.action.bulk.BulkProcessor;
+import org.elasticsearch.action.bulk.BulkRequest;
+import org.elasticsearch.action.bulk.BulkResponse;
+import org.elasticsearch.action.index.IndexRequest;
 import org.elasticsearch.client.RestHighLevelClient;
+import org.elasticsearch.common.unit.ByteSizeUnit;
+import org.elasticsearch.common.unit.ByteSizeValue;
 
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 import java.util.concurrent.atomic.AtomicInteger;
+import java.util.stream.Collectors;
+
+import static com.serli.oracle.of.bacon.repository.ElasticSearchRepository.ES_INDEX;
+import static com.serli.oracle.of.bacon.repository.ElasticSearchRepository.ES_TYPE;
+import static org.elasticsearch.common.xcontent.XContentFactory.jsonBuilder;
 
 public class CompletionLoader {
     private static AtomicInteger count = new AtomicInteger(0);
@@ -22,17 +36,71 @@ public class CompletionLoader {
         }
 
         String inputFilePath = args[0];
+
+        // Create bulk processor
+        BulkProcessor bulkProcessor = buildBulkProcessor(client);
+
         try (BufferedReader bufferedReader = Files.newBufferedReader(Paths.get(inputFilePath))) {
-            bufferedReader
-                    .lines()
-                    .forEach(line -> {
-                        //TODO ElasticSearch insert
-                        System.out.println(line);
-                    });
+            List<String> actorsNames = bufferedReader.lines().collect(Collectors.toList());
+
+            // Create an IndexRequest for each actor, and add it to the bulk processor
+            for (String actorName : actorsNames) {
+                IndexRequest indexRequest = toIndexRequestOfSuggestDoc(ES_INDEX, ES_TYPE, actorName);
+
+                count.incrementAndGet();
+                bulkProcessor.add(indexRequest);
+            }
         }
 
         System.out.println("Inserted total of " + count.get() + " actors");
 
+        bulkProcessor.flush();
+        bulkProcessor.close();
+        
         client.close();
+    }
+
+    /**
+     * Create a bulk processor for import to process IndexRequest by flushing every 8MB
+     */
+    private static BulkProcessor buildBulkProcessor(RestHighLevelClient client) {
+        return BulkProcessor.builder(client::bulkAsync, new BulkProcessor.Listener() {
+            @Override
+            public void beforeBulk(long executionId, BulkRequest request) {
+                System.out.println("Start bulk " + executionId);
+            }
+            @Override
+            public void afterBulk(long executionId, BulkRequest request, BulkResponse response) {
+                System.out.println("Bulk " + executionId + " succeeded");
+            }
+            @Override
+            public void afterBulk(long executionId, BulkRequest request, Throwable failure) {
+                System.out.println("Bulk " + executionId + " failed: " + failure.getMessage());
+            }
+        }).setBulkActions(-1).setBulkSize(new ByteSizeValue(8, ByteSizeUnit.MB)).build();
+    }
+
+    /**
+     * Create an IndexRequest of a basic document with suggestion for completion
+     */
+    private static IndexRequest toIndexRequestOfSuggestDoc(String esIndex, String esType, String name) throws IOException {
+        return new IndexRequest(esIndex, esType)
+            .source(
+                jsonBuilder()
+                    .startObject()
+                        .field("name", name)
+                        .field("suggest", generateSuggest(name))
+                    .endObject()
+            );
+    }
+
+    private static List<String> generateSuggest(String str) {
+        List<String> suggestions = new ArrayList<>();
+        List<String> strParts = Arrays.asList(str.split(" "));
+
+        suggestions.add(str);
+        suggestions.addAll(strParts);
+
+        return suggestions;
     }
 }
