457131f8dc2bb00bbc6d2988caad00cc298f1b10
Clément Garbay <clementgarbay@gmail.com>
2018-02-21 09:18:19 +0100

    Implement getActorByName using MongoDB, update API endpoint

--- oracle-of-bacon-backend/src/main/java/com/serli/oracle/of/bacon/repository/MongoDbRepository.java
+++ oracle-of-bacon-backend/src/main/java/com/serli/oracle/of/bacon/repository/MongoDbRepository.java
@@ -2,6 +2,7 @@ package com.serli.oracle.of.bacon.repository;
 
 import com.mongodb.MongoClient;
 import com.mongodb.client.MongoCollection;
+import com.mongodb.client.model.Filters;
 import org.bson.Document;
 
 import java.util.Optional;
@@ -9,12 +10,17 @@ import java.util.Optional;
 public class MongoDbRepository {
     private final MongoCollection<Document> actorCollection;
 
+    private static final String DATABASE = "workshop";
+    private static final String COLLECTION = "actors";
+
     public MongoDbRepository() {
-        this.actorCollection = new MongoClient("localhost", 27017).getDatabase("workshop").getCollection("actors");
+        this.actorCollection = new MongoClient("localhost", 27017)
+                .getDatabase(DATABASE)
+                .getCollection(COLLECTION);
     }
 
     public Optional<Document> getActorByName(String name) {
-        // TODO implement actor fetch
-        return null;
+        Document actor = this.actorCollection.find(Filters.eq("name", name)).first();
+        return Optional.ofNullable(actor);
     }
 }
